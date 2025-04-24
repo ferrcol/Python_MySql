570dd250f34d23cfc1d3ca0536620202cc46680b
ferrcol <colcombetfernando@gmail.com>
2025-04-25 00:11:11 +0200

    update and delete costumer

--- app_gym/costumer_dao.py
+++ app_gym/costumer_dao.py
@@ -48,12 +48,58 @@ class CostumerDAO:
                 cursor.close()
                 Conection.release_conection(conection)
 
+    @classmethod
+    def update(cls, costumer):
+        conection = None
+        try:
+            conection = Conection.get_conection()
+            cursor = conection.cursor()
+            values = (costumer.first_name, costumer.last_name, costumer.membership, costumer.id)
+            cursor.execute(cls.UPDATE, values)
+            conection.commit()
+            return cursor.rowcount
+
+
+        except Exception as e:
+            print(f"Error in update costumer: {e}")
+        finally:
+            if conection is not None:
+                cursor.close()
+                Conection.release_conection(conection)
+
+    @classmethod
+    def delete(cls, costumer):
+        conection = None
+        try:
+            conection = Conection.get_conection()
+            cursor = conection.cursor()
+            value = (costumer.id,)
+            cursor.execute(cls.DELETE, value)
+            conection.commit()
+            return cursor.rowcount
+
+
+        except Exception as e:
+            print(f"Error in delete costumer: {e}")
+        finally:
+            if conection is not None:
+                cursor.close()
+                Conection.release_conection(conection)
+
 
 
 if __name__ == "__main__":
-    costumer1 = Costumer(first_name="Clau", last_name="Smith", membership=255)
-    insert_costumers = CostumerDAO.insert(costumer1)
-    print(f"Inserted costumers: {insert_costumers}")
+    #costumer1 = Costumer(first_name="Clau", last_name="Smith", membership=255)
+    #insert_costumers = CostumerDAO.insert(costumer1)
+    #print(f"Inserted costumers: {insert_costumers}")
+
+    #costumer_update = Costumer(6,"Bart", "SmSimpson", 300)
+    #update_costumers = CostumerDAO.update(costumer_update)
+    #print(f"Update costumers: {update_costumers}")
+
+    costumer_delete = Costumer(id=5)
+    delete_costumers = CostumerDAO.delete(costumer_delete)
+    print(f"delete costumer: {delete_costumers}")
 
     costumers = CostumerDAO.select()
     for costumer in costumers:
